a5b24869a382590029af885559e46a783a6993dc
Emanuel Alasu <emanuel.alasu@gmail.com>
2013-06-23 12:50:53 -0700

    fixes, make it work with all-access tracks

--- sync-playlist.py
+++ sync-playlist.py
@@ -1,8 +1,11 @@
 
 import os
+import shutil
 from getpass import getpass
 import keyring
 from ConfigParser import SafeConfigParser
+from pprint import pprint
+import unicodedata
 
 from gmusicapi.clients import Webclient, Musicmanager
 from mutagen.easyid3 import EasyID3
@@ -17,57 +20,56 @@ def find_dict(lst, criteria):
     return None
 
 def escape_path(fname):
-    fname = fname.encode('ascii', 'replace')
     for c in ':/\\?"\'<>|*': 
         fname = fname.replace(c, '_')
     return fname
 
-#def setup_id3_keys():
-#    def registerTimestamp(key, frameid):
-#        def getter(id3, key):
-#            return list(ts.text for ts in id3[frameid].text)
-#        def setter(id3, key, value):
-#            if isinstance(value, str) or isinstance(value,unicode): value = [value]
-#            try:
-#                frame = id3[frameid]
-#            except KeyError:
-#                id3.add(mutagen.id3.Frames[frameid](encoding=3, text=list(ID3TimeStamp(ts) for ts in value)))
-#            else:
-#                frame.encoding = 3
-#                frame.text = list(ID3TimeStamp(ts) for ts in value)
-#        def deleter(id3, key):
-#            del(id3[frameid])
-#        EasyID3.RegisterKey(key, getter, setter, deleter)
-#    
-#    registerTimestamp("year", "TDRC")
-#    registerTimestamp("releasedate", "TDRL")
-#
-#setup_id3_keys()
-#
-#def _copy_track_metadata(file_name, track):
-#    def do_copy():
-#        mp3 = EasyID3()
-#        for m_k, g_k in {
-#            'name': 'title', 
-#            'album': 'album', 
-#            'genre': 'genre', 
-#            'artist': 'artist', 
-#            'albumArtist': 'performer',
-#            'track': 'tracknumber', 
-#            'disc': 'discnumber', 
-#            'year': 'date'
-#        }.iteritems():
-#            if m_k in track:
-#                value = unicode(track[m_k])
-#                if m_k=='album_image':
-#                    value = self._get_album_image(value)
-#                if m_k=='track':
-#                    value = '{0}/{1}'.format(value, track['totalTracks'])
-#                if m_k=='disc':
-#                    value = '{0}/{1}'.format(value, track['totalDiscs'])
-#                mp3[g_k] = value
-#        mp3.save(file_name)
-#    do_copy()
+def setup_id3_keys():
+    def registerTimestamp(key, frameid):
+        def getter(id3, key):
+            return list(ts.text for ts in id3[frameid].text)
+        def setter(id3, key, value):
+            if isinstance(value, str) or isinstance(value,unicode): value = [value]
+            try:
+                frame = id3[frameid]
+            except KeyError:
+                id3.add(mutagen.id3.Frames[frameid](encoding=3, text=list(ID3TimeStamp(ts) for ts in value)))
+            else:
+                frame.encoding = 3
+                frame.text = list(ID3TimeStamp(ts) for ts in value)
+        def deleter(id3, key):
+            del(id3[frameid])
+        EasyID3.RegisterKey(key, getter, setter, deleter)
+    
+    registerTimestamp("year", "TDRC")
+    registerTimestamp("releasedate", "TDRL")
+
+setup_id3_keys()
+
+def _copy_track_metadata(file_name, track):
+    def do_copy():
+        mp3 = EasyID3()
+        for m_k, g_k in {
+            'name': 'title',
+            'album': 'album',
+            'genre': 'genre',
+            'artist': 'artist',
+            'albumArtist': 'performer',
+            'track': 'tracknumber',
+            'disc': 'discnumber',
+            'year': 'date',
+        }.iteritems():
+            if m_k in track:
+                value = unicode(track[m_k])
+                if m_k=='album_image':
+                    value = self._get_album_image(value)
+                if m_k=='track':
+                    value = '{0}/{1}'.format(value, track.get('totalTracks', 1))
+                if m_k=='disc':
+                    value = '{0}/{1}'.format(value, track.get('totalDiscs', 1))
+                mp3[g_k] = value
+        mp3.save(file_name)
+    do_copy()
 
 
 # get playlist, compare with local, download new tracks, optionally delete local files not in the playlist
@@ -81,12 +83,12 @@ class PlaylistSync:
  
     def _login_wc(self):
         APP_NAME = 'gmusic-sync-playlist'
+        CONFIG_FILE = 'auth.cfg'
 
-        config_file = 'auth_demo.cfg'
         config = SafeConfigParser({
             'username':'',
         })
-        config.read(config_file)
+        config.read(CONFIG_FILE)
         if not config.has_section('auth'):
             config.add_section('auth')
     
@@ -105,7 +107,7 @@ class PlaylistSync:
                     print "Sign-on failed."
     
             config.set('auth', 'username', username)
-            with open(config_file, 'wb') as f:
+            with open(CONFIG_FILE, 'wb') as f:
                 config.write(f)
     
             keyring.set_password(APP_NAME, username, password)
@@ -122,9 +124,9 @@ class PlaylistSync:
         if not albumartist:
             albumartist = 'Various'
         file_name = escape_path(u'{track:02d} {name}.mp3'.format(**track))
-        if track['totalDiscs'] > 1:
+        if track.get('totalDiscs', 1) > 1:
             file_name = u'{disc}-'.format(**track) + file_name
-        return os.path.join(self.root, escape_path(albumartist), escape_path(track['album']), file_name)
+        return unicodedata.normalize('NFD', os.path.join(self.root, escape_path(albumartist), escape_path(track['album']), file_name))
 
     def get_local_tracks(self):
         # return (metadata, file_name) of all files in root
@@ -141,7 +143,7 @@ class PlaylistSync:
                     #  'track': id3['tracknumber'],
                     #  'disc': id3['discnumber']
                     #}
-                    yield file_name, track
+                    yield unicodedata.normalize('NFD', file_name.decode('utf-8')), track
 
     def get_playlist_tracks(self):
         # return (metadata, local_file_name) for each track in playlist
@@ -151,7 +153,6 @@ class PlaylistSync:
             playlist_id, = user_playlists[self.playlist_name]
         except KeyError:
             raise Exception('playlist "{0}" not found'.format(self.playlist_name))
-        print playlist_id
         for track in self.wc.get_playlist_songs(playlist_id):
             yield self.track_file_name(track), track
 
@@ -159,14 +160,15 @@ class PlaylistSync:
         # download track from gmusic, write to file_name
         if not os.path.exists(os.path.dirname(file_name)):
             os.makedirs(os.path.dirname(file_name))
-        #url = self.wc.get_stream_url(track['id'])
-        #r = requests.get(url)
-        #with open(file_name, 'wb') as f:
-        #    f.write(r.content)
-        #_copy_track_metadata(file_name, track)
-        fn, audio = self.mm.download_song(track['id'])
-        with open(file_name, 'wb') as f:
-            f.write(audio)
+        if track['type'] == 7:
+            audio = self.wc.get_stream_audio(track['id'])
+            with open(file_name, 'wb') as f:
+                f.write(audio)
+            _copy_track_metadata(file_name, track)
+        else:
+            fn, audio = self.mm.download_song(track['id'])
+            with open(file_name, 'wb') as f:
+                f.write(audio)
         
     def remove_track(self, file_name):
         """Removes the track and walks up the tree deleting empty folders
@@ -188,10 +190,13 @@ class PlaylistSync:
 
         to_add = []
         to_remove = []
+        to_rename = []
 
         for file_name, track in sorted(playlist.iteritems()):
-            if file_name not in local:
+            if file_name not in local and file_name.encode('ascii', 'replace').replace('?','_') not in local:
                 to_add.append((track, file_name))
+            elif file_name not in local and file_name.encode('ascii', 'replace').replace('?','_') in local:
+                to_rename.append((file_name.encode('ascii', 'replace').replace('?','_'), file_name))
 
         if remove:
             for file_name, track in sorted(local.iteritems()):
@@ -202,16 +207,28 @@ class PlaylistSync:
             print 'Deleting tracks:'
             for track, file_name in to_remove:
                 print '  ' + file_name
+            print ''
         if to_add:
             print 'Adding tracks:'
             for track, file_name in to_add:
                 print '  ' + file_name
+            print ''
+        if to_rename:
+            print 'Renaming tracks:'
+            for src, dst in to_rename:
+                print '  {0} to {1}'.format(src, dst)
+            print ''
         if not (to_add or to_remove):
             print 'Nothing to do.'
+            print ''
 
         if confirm:
             raw_input('Press enter to proceed')
 
+        for src, dst in to_rename:
+            if not os.path.exists(os.path.dirname(dst)):
+                os.makedirs(os.path.dirname(dst))
+            shutil.move(src, dst)
         for track, file_name in to_remove:
             print 'removing track ' + file_name
             self.remove_track(file_name)
